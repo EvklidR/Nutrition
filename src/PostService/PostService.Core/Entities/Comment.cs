4216accc41869a54b58dd05ba95f8bfe1591c3f9
E <gbvu2004@gmail.com>
2025-01-17 19:05:46 +0300

    clean post service

--- src/PostService/PostService.Core/Entities/Comment.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PostService.Core.Entities
-{
-    public class Comment
-    {
-        public string Text { get; set; }
-        public List<string> UserLikeIds { get; set; } = [];
-        public List<Comment> SubComments { get; set; } = [];
-    }
-}
